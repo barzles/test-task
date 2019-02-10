fd1f083511c17a85414935802267dd20aceb0312
barzles <barzles97@gmail.com>
2019-02-11 00:36:51 +0200

    last up

--- js/custom.js
+++ js/custom.js
@@ -1,12 +1,13 @@
 (function ($) {
 
-  $(document).ready(function() {
-      $('body').niceScroll();
-  });
 
-  $('.header-menu__btn').on('click', function() {
-    $(this).toggleClass('active');
-    $('.menu').toggleClass('active');
-  });
+	$(document).ready(function() {
+		$('body').niceScroll();
+	});
+
+	$('.header-menu__btn').on('click', function () {
+		$(this).toggleClass('active');
+		$('.menu').toggleClass('active');
+	});
 
 })(jQuery);
